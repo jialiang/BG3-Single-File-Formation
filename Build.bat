4b4ad232411086879bb4160f395463a4fef40c56
Jia Liang <hojialiang@protonmail.com>
2026-03-17 03:28:10 +0800

    Fix broken build process

--- Build.bat
+++ Build.bat
@@ -2,8 +2,17 @@
 setlocal
 
 set DIVINE=%~dp0LSLib\Packed\Tools\Divine.exe
-set SOURCE=%~dp0Mods
+set TEMP_DIR=%~dp0Temp
+set SOURCE=%~dp0Temp
 set OUTPUT=%~dp0SingleFileFormation.pak
+
+if exist "%TEMP_DIR%" rmdir /s /q "%TEMP_DIR%"
+xcopy /e /i /q "%~dp0Mods" "%TEMP_DIR%\Mods"
+
+if errorlevel 1 (
+    echo Copy failed
+    goto cleanup
+)
 
 "%DIVINE%" --action create-package --source "%SOURCE%" --destination "%OUTPUT%" --game bg3
 
@@ -12,3 +21,8 @@ if errorlevel 1 (
 ) else (
     echo Build successful: %OUTPUT%
 )
+
+:cleanup
+
+rmdir /s /q "%TEMP_DIR%"
+pause
